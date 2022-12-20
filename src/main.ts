c230af40bee77a4eec25e55480dc976a33b4dd25
Daniel Brown <danielbrown25.9.2@gmail.com>
2022-12-20 20:37:47 -0300

    microservice test

--- src/main.ts
+++ src/main.ts
@@ -6,43 +6,43 @@ import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { AllExceptionsFilter } from './common/filters/httpException.filter';
 import { Logger } from '@nestjs/common';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 
 const logger = new Logger('Microservice');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // add the interceptors and filters
+  await app.listen(process.env.PORT || 3000).then(() => {
+    logger.log(`Main service running on port ${process.env.PORT || 3000}...`);
+  });
+
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalInterceptors(new TimeoutInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
 
   // Microservice listener
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [
-        'amqp://' +
-          process.env.RMQ_USER +
-          ':' +
-          process.env.RMQ_PASSWORD +
-          '@' +
-          process.env.RMQ_URL,
-      ],
-      queue: process.env.RMQ_QUEUE,
-      noAck: false,
+  const microservice = await NestFactory.createMicroservice(
+    AppMicroserviceModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          'amqp://' +
+            process.env.RMQ_USER +
+            ':' +
+            process.env.RMQ_PASSWORD +
+            '@' +
+            process.env.RMQ_URL,
+        ],
+        queue: process.env.RMQ_QUEUE,
+        noAck: false,
+      },
     },
-  });
+  );
 
-  // create a microservice hybrid app
-  await app.startAllMicroservices();
-  await app.listen(process.env.PORT || 3000).then(() => {
-    logger.log(
-      `Main service running on port ${
-        process.env.PORT || 3000
-      }, and listening on ${process.env.RMQ_QUEUE}...`,
-    );
+  await microservice.listen().then(() => {
+    logger.log(`Microservice is listening on ${process.env.RMQ_QUEUE}...`);
   });
 }
 bootstrap();
